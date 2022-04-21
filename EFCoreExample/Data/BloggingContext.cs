01cabda7211594fe3bde90b8324d5dfea2f33cb9
SheiDominguez <sgdominguezj@gmail.com>
2022-04-21 12:33:39 -0500

    Primer commit
    Inicio del proyecto para conocer Entity Framework

---
+++ EFCoreExample/Data/BloggingContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.Data
+{
+    public class BloggingContext : DbContext
+    {
+        // DbSet - para combertir nuestro modelo en una tabla
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        // Crear la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Cadena de conexión
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Blogging; Integrated Security=True");
+        }
+
+    }
+}
---
+++ EFCoreExample/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EFCoreExample
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        // Propiedad de navegación: Con esto se hace la referencia a la otra tabla (Foreign Key)
+        public int BlogId { get; set; }
+        // Con esto podríamos decir algo como post.Blog.Url
+        public Blog Blog { get; set; }
+    }
+}
